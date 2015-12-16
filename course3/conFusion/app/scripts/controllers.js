5d70f8804cb1c0e3e90d8b858531738d4860f7bd
Jakob Wanner <deadzac@gmx.net>
2015-12-16 07:23:00 +0100

    Added routes

--- course3/conFusion/app/scripts/controllers.js
+++ course3/conFusion/app/scripts/controllers.js
@@ -65,8 +65,8 @@ app.controller('FeedbackController', ['$scope', function($scope) {
     };
 }]);
 
-app.controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
-    $scope.dish = menuFactory.getDish(3);
+app.controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
+    $scope.dish = menuFactory.getDish(parseInt($routeParams.id, 10));
 }]);
 
 app.controller('DishCommentController', ['$scope', function($scope) {
